10485c186a6fa5dc8fa2604a725479f9ecaf897a
kosmastsk <kosmastsk@gmail.com>
2016-09-15 18:19:57 +0300

    minified js

--- js/loading.js
+++ js/loading.js
@@ -1,12 +1,1 @@
-
-//run the svg animation for 5000 miliseconds
-// Wait for window load
- $(window).load(function() {
-    // Animate loader off screen
-    $(".loading").fadeOut(2000);
-    $('html, body').css({
-    'overflow': 'auto',
-    'height': 'auto'
-});
-    $('#allbtn').trigger('click');
-  });
+$(window).load(function(){$(".loading").fadeOut(2000);$('html, body').css({'overflow':'auto','height':'auto'});$('#allbtn').trigger('click');});
